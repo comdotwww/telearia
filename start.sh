e0005f931e84d9f6da17e3f6520cc4b1d289ca7b
comdotwww <62149924+comdotwww@users.noreply.github.com>
2025-02-14 04:08:45 +0800

    update

--- start.sh
+++ start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
-touch ./aria2.session
-aria2c --conf-path=./aria2.conf -D
+if [ ! -f "/app/aria2.session" ];then
+  touch /app/aria2.session
+fi
+aria2c --conf-path=/app/aria2.conf -D
 exec node index.js
